c17a2e0d382fe83a1921aa45277690bf8c6e2b8d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-19 13:37:03 -0700

    initial commit

---
+++ practice.py
@@ -0,0 +1,10 @@
+from twilio.rest import Client
+import os
+
+account_sid = os.environ['TWILIO_SID']
+auth_token = os.environ['TWILIO_TOKEN']
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+        to=)
