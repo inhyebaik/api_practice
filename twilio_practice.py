2354b7274b55bd16448b0ba60622252994d794dc
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-19 16:17:10 -0700

    able to send sms now

--- twilio_practice.py
+++ twilio_practice.py
@@ -3,10 +3,13 @@ import os
 
 account_sid = os.environ['TWILIO_SID']
 auth_token = os.environ['TWILIO_TOKEN']
-
+my_num = "+16173197517"
+twilio_num = "+16179348427"
+test_num = "+15005550006"
 
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-        to=)
+message = client.messages.create(to=my_num, 
+                                from_=twilio_num, 
+                                body="Hey there")
